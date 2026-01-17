e84e081114c8f2c28c66408863c4ca5eed0642f9
santosh456789 <97775456+santosh456789@users.noreply.github.com>
2026-01-17 19:15:46 +0530

    Update riskEngines.js

--- riskEngines.js
+++ riskEngines.js
@@ -1,12 +1,39 @@
 function metabolicRisk(d) {
   let bmi = d.body.weightKg / ((d.body.heightCm / 100) ** 2);
-  return bmi > 25 ? 0.7 : 0.3;
+  return (
+    (bmi > 25 ? 0.6 : 0.2) +
+    (d.lifestyle.exercise === "none" ? 0.6 : 0.2)
+  ) / 2;
 }
 
 function gutRisk(d) {
-  return map.gas[d.digestion.gasBloating];
+  return (
+    map.gas[d.digestion.gasBloating] +
+    (d.digestion.motionFrequency !== "daily" ? 0.6 : 0.2)
+  ) / 2;
+}
+
+function hydrationRisk(d) {
+  return (
+    (d.hydration.waterLiters < 2 ? 0.6 : 0.2) +
+    map.urineColor[d.hydration.urineColor]
+  ) / 2;
 }
 
 function nutritionRisk(d) {
-  return (map.hairFall[d.body.hairFall] + map.energy[d.body.energy]) / 2;
+  return (
+    map.hairFall[d.body.hairFall] +
+    map.energy[d.body.energy]
+  ) / 2;
+}
+
+function musculoskeletalRisk(d) {
+  return map.jointPain[d.body.jointPain];
+}
+
+function lifestyleRisk(d) {
+  return (
+    map.sleep[d.lifestyle.sleep] +
+    (d.lifestyle.exercise === "none" ? 0.6 : 0.2)
+  ) / 2;
 }
