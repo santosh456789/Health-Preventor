9ab3aec49849d6c8b466aba7098780bf9949a224
santosh456789 <97775456+santosh456789@users.noreply.github.com>
2026-01-17 19:15:14 +0530

    Update dataModel.js

--- dataModel.js
+++ dataModel.js
@@ -1,17 +1,32 @@
 function buildHealthInput() {
   return {
     age: +document.getElementById("age").value,
+
     body: {
       heightCm: +document.getElementById("height").value,
       weightKg: +document.getElementById("weight").value,
       hairFall: document.getElementById("hairFall").value,
-      energy: document.getElementById("energy").value
+      energy: document.getElementById("energy").value,
+      jointPain: document.getElementById("jointPain").value
     },
-    digestion: {
-      gasBloating: document.getElementById("gas").value
-    },
+
     hydration: {
-      waterLiters: +document.getElementById("water").value
+      waterLiters: +document.getElementById("water").value,
+      urineColor: document.getElementById("urineColor").value
+    },
+
+    digestion: {
+      gasBloating: document.getElementById("gas").value,
+      motionFrequency: document.getElementById("motion").value
+    },
+
+    diet: {
+      oilyFood: document.getElementById("oilyFood").value
+    },
+
+    lifestyle: {
+      exercise: document.getElementById("exercise").value,
+      sleep: document.getElementById("sleep").value
     }
   };
 }
